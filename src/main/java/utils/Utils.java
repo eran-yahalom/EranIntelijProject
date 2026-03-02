09679d8aac49c4f1c17d2d4322163118de839695
Eran Yahalom <eran.yahalom@driivz.com>
2026-03-02 09:16:32 +0200

    commit #2- add methods to utils class

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class Utils {
 
@@ -46,7 +45,7 @@ public class Utils {
         return fullText.replaceAll("\\D+", "");
     }
 
-    public static boolean addProductToCart(WebDriver driver,List<WebElement> titles, List<WebElement> buttons, String productName) {
+    public static boolean addProductToCart(WebDriver driver, List<WebElement> titles, List<WebElement> buttons, String productName) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(titles.get(titles.size() - 1)));
         for (int i = 0; i < titles.size(); i++) {
@@ -60,14 +59,67 @@ public class Utils {
 
     public static boolean isMessageDisplayedCorrectly(WebDriver driver, WebElement element, String propertyKey) {
         try {
-            System.out.println("aaaa:"+element.getText());
+            System.out.println("aaaa:" + element.getText());
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             wait.until(ExpectedConditions.visibilityOf(element));
-            System.out.println("xxxxx:"+element.getText());
+            System.out.println("xxxxx:" + element.getText());
             return element.getText().equalsIgnoreCase(readProperty(propertyKey));
         } catch (TimeoutException e) {
             return false;
         }
+    }
+
+    public List<Double> getPricesFromUI(List<WebElement> priceElements) {
+
+        List<Double> prices = new ArrayList<>();
+
+        for (WebElement element : priceElements) {
+            String priceText = element.getText()
+                    .replaceAll("[^0-9.]", "");
+            prices.add(Double.parseDouble(priceText));
+        }
+
+        return prices;
+    }
+
+    public List<Double> verifyPricesSortedHighToLow(List<Double> actualPrices) {
+
+        List<Double> expectedPrices = new ArrayList<>(actualPrices);
+        expectedPrices.sort(Comparator.reverseOrder());
+        return expectedPrices;
+    }
+
+    public List<Double> verifyPricesSortedLowToHigh(List<Double> actualPrices) {
+
+        List<Double> expectedPrices = new ArrayList<>(actualPrices);
+        expectedPrices.sort(Comparator.naturalOrder());
+        return expectedPrices;
+    }
+
+    public List<String> getProducesNamesFromUI(List<WebElement> productNameElements) {
+
+        List<String> productNames = new ArrayList<>();
+        for (WebElement element : productNameElements) {
+            String productName = element.getText();
+            productNames.add(productName);
+        }
+        return productNames;
+    }
+
+    public List<String> sortItemNamesHighToLow(List<String> actualNames) {
+        actualNames.sort(Collections.reverseOrder());
+
+        return actualNames;
+    }
+
+    public List<String> sortItemNamesRegularOrder(List<String> actualNames) {
+        Collections.sort(actualNames);
+        return actualNames;
+    }
+
+    public boolean areItemsDisplayedAInCorrectOrder(List<WebElement> elements) {
+        int size = elements.size();
+        return size > 0;
     }
 }
 
