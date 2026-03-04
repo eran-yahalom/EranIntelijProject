bddc8afe185806a28d49fc9893a9aff2ad04ef36
Eran Yahalom <eran.yahalom@driivz.com>
2026-03-04 11:59:40 +0200

    commit #12 add launchers to project

---
+++ src/test/java/launchers/CartLauncher.java
@@ -0,0 +1,23 @@
+package launchers;
+
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"step_definitions"},
+        plugin = {
+                "pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "json:target/cucumber.json"
+        },
+        tags = "@cart or @ui"
+)
+
+public class CartLauncher extends BaseLauncher{
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
